77cb150bfbc78609718303384a3c0ca21ee51149
dstN <github@yinside.de>
2022-06-09 15:22:24 +0000

    Changed main.js for bubble import

--- src/main.js
+++ src/main.js
@@ -1,14 +1,18 @@
 // Child Bot:
 import 'dotenv/config';
+import fs from 'fs';
 import * as tweetService from './tweet/tweet.service';
+import * as bubbleImporter from './bubble/bubble.importer';
 import * as controller from './controller';
 const CronJob = require('cron').CronJob;
+
+bubbleImporter.starter('testlist.txt', process.env.BUBBLE_NAME, process.env.BUBBLE_DESC);
 
 async function test() {
 	let next = await tweetService.getNextTweet();
 }
 
-test();
+//test();
 
 const job = new CronJob(
 	'*/10 * * * * *',
