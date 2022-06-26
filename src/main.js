c2a03971f83d792df7a0af67a2e8fb93e24aabec
dstN <github@yinside.de>
2022-06-26 22:23:47 +0200

    Added userHandler, listcompare, checkThreshhold

--- src/main.js
+++ src/main.js
@@ -1,20 +1,15 @@
 // Child Bot:
 import 'dotenv/config';
-import fs from 'fs';
+//import fs from 'fs';
 //import * as tweetService from './tweet/tweet.service';
 //import * as bubbleImporter from './bubble/bubble.importer';
-import * as apiService from './twitter-api/api.service';
+import * as userHandler from './user/user.handler';
 const CronJob = require('cron').CronJob;
 
 //bubbleImporter.starter('testlist.txt', process.env.BUBBLE_NAME, process.env.BUBBLE_DESC);
 
-
-async function userHandler(id) {
-	let followings = await apiService.getFollowings(id);
-	console.table(followings);
-}
-
-//userHandler(process.env.EXAMPLE_USRID);
+//userHandler.handler(process.env.EXAMPLE_USRID);
+userHandler.handler(process.env.EXMPL);
 //userHandler('1337');
 
 const job = new CronJob(
--- src/user/user.handler.js
+++ src/user/user.handler.js
@@ -1,8 +1,69 @@
 import * as userService from '../user/user.service';
-import * as bubbleService from './bubble.service';
-import * as twitterService from '../twitter-api/api.service';
+import * as bubbleService from '../bubble/bubble.service';
+import * as apiService from '../twitter-api/api.service';
 
 export async function handler(id) {
-	let token = twitterService.botCache.take('token');
-	token = token.next_token;
+	let followings = await apiService.getFollowings(id);
+	let bubbleMembers = await bubbleService.getBubbleMembers(process.env.BUBBLEID);
+	let userAdded = await listCompare(followings, bubbleMembers);
+	let userExist = await userService.getUserById(id);
+	if (userAdded && !userExist.length) {
+		let userObject = {
+			id: id,
+			handle: await apiService.getUserHandle(id),
+			bubble: [{ id: process.env.BUBBLEID }],
+			rating: 0,
+		};
+		await userService.postUser(userObject);
+	} else if (userAdded && userExist.length) {
+		let updateObject = {
+			rating: userExist.rating + 1,
+		};
+		await userService.updateUser(id, updateObject);
+	}
+}
+
+async function listCompare(followings, bubbleMembers) {
+	let positives = 0;
+	let followLength = followings.length;
+	let percentage = 0;
+
+	for (const follow of followings) {
+		let followId = follow.id;
+		let userDoesFollow = bubbleMembers.filter((userObject) => userObject.id === followId).length;
+
+		if (!userDoesFollow) {
+			continue;
+		}
+
+		positives++;
+	}
+
+	percentage = positives / (followLength / 100);
+
+	let requiredPercentage = -1;
+
+	if (followLength <= 10) {
+		requiredPercentage = 50;
+	} else if (followLength <= 20) {
+		requiredPercentage = 25;
+	} else if (followLength <= 50) {
+		requiredPercentage = 15;
+	} else {
+		if (positives >= 15 || percentage >= 10) {
+			return true;
+		} else {
+			requiredPercentage = 10;
+		}
+	}
+
+	return await checkTreshhold(requiredPercentage, percentage);
+}
+
+async function checkTreshhold(requiredPercentage, percentage) {
+	if (percentage >= requiredPercentage) {
+		return true;
+	}
+
+	return false;
 }
